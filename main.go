967809d7b94efc10bcfb937ffe8c90619817c577
Ariya Wildan <ariya.devanto@sinarmasmining.com>
2021-01-04 10:42:18 +0700

    fix(email): change env and smtp

--- main.go
+++ main.go
@@ -1,28 +1,70 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"net/smtp"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"gopkg.in/gomail.v2"
 )
 
 var (
-	user = os.Getenv("MAIL_RELAY_SMTP_USER")
-	pass = os.Getenv("MAIL_RELAY_SMTP_PASS")
-	host = os.Getenv("MAIL_RELAY_SMTP_HOST")
-	port = os.Getenv("MAIL_RELAY_SMTP_PORT")
-	from = os.Getenv("MAIL_RELAY_FROM_EMAIL")
+	user string
+	pass string
+	host string
+	port string
+	from string
 )
+
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+	userName, exists := os.LookupEnv("USERNAMEEMAIL")
+	if exists {
+		user = userName
+		log.Println(user)
+	}
+	passUser, exists := os.LookupEnv("PASSWORD")
+	if exists {
+		pass = passUser
+		log.Println(pass)
+	}
+	hostUser, exists := os.LookupEnv("HOST")
+	if exists {
+		host = hostUser
+		log.Println(host)
+	}
+	portUser, exists := os.LookupEnv("PORTEMAIL")
+	if exists {
+		port = portUser
+		log.Println(port)
+	}
+	formUser, exists := os.LookupEnv("FROM")
+	if exists {
+		from = formUser
+		log.Println(from)
+	}
+
+}
 
 func main() {
 	r := gin.Default()
+	r.GET("/", hello)
 	r.GET("/health", health)
 	r.POST("/api.sendgrid.com/v3/mail/send", sendgrid)
 	r.POST("/api.sendinblue.com/v3/smtp/email", sendinblue)
 	r.Run()
+}
+
+func hello(c *gin.Context) {
+	c.JSON(200, gin.H{"status": "Hallo"})
 }
 
 func health(c *gin.Context) {
@@ -31,14 +73,18 @@ func health(c *gin.Context) {
 
 func sendgrid(c *gin.Context) {
 	var body Sendgrid
+	if c.Request.Header["Authorization"] == nil {
+		c.JSON(http.StatusBadRequest, gin.H{})
+	}
+	token := strings.Split(c.Request.Header["Authorization"][0], " ")[1]
+	log.Println(token)
 	if err := c.ShouldBindJSON(&body); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-
 	to := mapPersonToEmail(body.Personalizations[0].To)
 	cc := []string{}
-	sendMail(to, cc, body.Subject, body.Content.Value)
+	sendMailv2(to, cc, body.Personalizations[0].Subject, body.Content[0].Value)
 	c.JSON(http.StatusAccepted, gin.H{})
 }
 
@@ -53,6 +99,19 @@ func sendinblue(c *gin.Context) {
 	cc := []string{}
 	sendMail(to, cc, body.Subject, body.HTMLContent)
 	c.JSON(http.StatusAccepted, gin.H{})
+}
+func sendMailv2(to []string, cc []string, subject, message string) {
+	auth := smtp.PlainAuth("", user, pass, host)
+
+	to = []string{to[0]}
+	msg := []byte("From:" + from + "\r\n" +
+		"To: " + to[0] + "\r\n" +
+		"Subject: " + subject + "\r\n" +
+		"\r\n" + message + "\r\n")
+	err := smtp.SendMail(host+":"+port, auth, from, to, msg)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func sendMail(to []string, cc []string, subject, message string) error {
@@ -94,12 +153,13 @@ type Sendgrid struct {
 	Personalizations []Personalizations `json:"personalizations"`
 	From             Person             `json:"from"`
 	Subject          string             `json:"subject"`
-	Content          SendgridContent    `json:"content"`
+	Content          []SendgridContent  `json:"content"`
 }
 
 // Personalizations for Sendgrid
 type Personalizations struct {
-	To []Person `json:"to"`
+	To      []Person `json:"to"`
+	Subject string   `json:subject`
 }
 
 // SendgridContent for email body
